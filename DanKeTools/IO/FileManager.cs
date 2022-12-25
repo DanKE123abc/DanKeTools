0028697d8c15b8630acfe251ec8c599b7a7aeeb7
DanKe <danke1024@foxmail.com>
2022-12-25 18:16:53 +0800

    update

--- DanKeTools/IO/FileManager.cs
+++ DanKeTools/IO/FileManager.cs
@@ -6,6 +6,10 @@ using UnityEngine.Events;
 using DanKeTools;
 using DanKeTools.Mono;
 
+using System.Text;
+using System.Xml;
+using UnityEngine.U2D;
+
 namespace DanKeTools.IO
 {
     
@@ -95,11 +99,109 @@ namespace DanKeTools.IO
                 //直接传给方法
                 callback(r.asset as T);
             }
+        }  
+        
+        /// <summary>
+        /// 加载Sprite
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static Sprite LoadSprite(string path)
+        {
+            return Resources.Load<Sprite>(path);
         }
 
+        /// <summary>
+        /// 加载指定路径图集中的指定名称图片
+        /// </summary>
+        /// <param name="_atlasPath"></param>
+        /// <param name="_spriteName"></param>
+        /// <returns></returns>
+        public static Sprite LoadSprite(string _atlasPath, string _spriteName)
+        {
+            Sprite tempSprite = null;
+            SpriteAtlas tempAtlas = GetSpriteAtlas(_atlasPath);
+            Debug.Log(_atlasPath + ".." + (tempAtlas == null));
+            if (tempAtlas != null) tempSprite = tempAtlas.GetSprite(_spriteName);
+            return tempSprite;
+        }
+        
+        /// <summary>
+        /// 已加载图集记录
+        /// </summary>
+        private static Dictionary<string, SpriteAtlas> m_UISpriteAtlasDic = new Dictionary<string, SpriteAtlas>();
+        /// <summary>
+        /// 获取一个图集
+        /// </summary>
+        /// <param name="atlasName"></param>
+        /// <returns></returns>
+        public static SpriteAtlas GetSpriteAtlas(string atlasName)
+        {
+            if (m_UISpriteAtlasDic.ContainsKey(atlasName))
+            {
+                if (m_UISpriteAtlasDic[atlasName] == null)
+                    m_UISpriteAtlasDic[atlasName] = Resources.Load<SpriteAtlas>(atlasName);
+            }
+            else
+            {
+                m_UISpriteAtlasDic.Add(atlasName, Resources.Load<SpriteAtlas>(atlasName));
+            }
 
+            return m_UISpriteAtlasDic[atlasName];
+        }
+
+        /// <summary>
+        /// 加载材质球
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static Material LoadMaterial(string path)
+        {
+            return Resources.Load<Material>(path);
+        }
+
+        /// <summary>
+        /// 加载贴图
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static Texture LoadTexture(string path)
+        {
+            return Resources.Load<Texture>(path);
+        }
+
+        /// <summary>
+        /// 加载XML
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static string LoadXML(string path)
+        {
+            XmlDocument Xdoc = new XmlDocument();
+            Debug.Log("当前目录是：" + path); //Application.dataPath);
+            //加载XML 文件
+            Xdoc.Load(path + ".xml");
+
+            //获取跟节点
+            XmlElement root = Xdoc.DocumentElement;
+            Debug.Log("根元素是：" + root.Name);
+
+            // data : 下一个子节点的名称 
+            //XmlNode dataNode = root.SelectSingleNode("data");  //获取根节点下的子节点
+            //Debug.Log("节点名称"+dataNode.Name);
+
+            StringBuilder content = new StringBuilder();
+
+            // 读取到的字符串
+            for (int i = 0; i < root.ChildNodes.Count; i++)
+            {
+                content.Append(root.ChildNodes[i].InnerText + ",");
+            }
+
+            return content.ToString();
+        }
+        
+        
     }
     
-    
-
 }
--- DanKeTools/UI/UIBasePanel.cs
+++ DanKeTools/UI/UIBasePanel.cs
@@ -4,72 +4,77 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-///<summary>
-///脚本名称： UIBasePanel.cs
-///修改时间：2022/12/6
-///脚本功能：UI基类面板
-///备注：
-///</summary>
-
 namespace DanKeTools.UI
 {
 
-public class UIBasePanel : MonoBehaviour
-{
-    //通过里式转换原则，来存储所有的UI控件
-    private Dictionary<string, List<UIBehaviour>> controlDic = new Dictionary<string, List<UIBehaviour>>();
-    private void Awake() 
-    {
-    //控件并保存在字典中，可根据自己UI搭载的控件更改
-      FinChildControl<Button>();
-      FinChildControl<Image>();
-      FinChildControl<Scrollbar>();
-      FinChildControl<Text>();
-    }
-    private void FinChildControl<T>() where T : UIBehaviour
-    {
-        T[] controls = this.GetComponentsInChildren<T>();//注意用的是GetComents,返回的是一组
-        string objName;//使用名字对控件按物体分类
-        for (int i = 0; i < controls.Length; i++)
-        { 
-            objName = controls[i].gameObject.name;
-            if(controlDic.ContainsKey(objName))
-            {
-                controlDic[objName].Add(controls[i]);//注意controlDic中存储的类型为<string, List<UIBehaviour>,所以我们的同一个UI的所有控件都在这个链表中
-            }
-            else
-            {
-                controlDic.Add(objName,new List<UIBehaviour>(){controls[i]});//添加UI到字典中
-            }
-        }     
-    }
-   
+    ///<summary>
+    ///脚本名称： UIBasePanel.cs
+    ///修改时间：2022/12/6
+    ///脚本功能：UI基类面板
+    ///备注：
+    ///</summary>
 
-    protected T GetControl<T>(string controlName) where T : UIBehaviour
+    public class UIBasePanel : MonoBehaviour
     {
-        if (controlDic.ContainsKey(controlName))
+        //通过里式转换原则，来存储所有的UI控件
+        private Dictionary<string, List<UIBehaviour>> controlDic = new Dictionary<string, List<UIBehaviour>>();
+
+        private void Awake()
         {
-            for(int i=0;i<controlDic[controlName].Count;i++)
-            {	
-                if (controlDic[controlName][i] is T)//使用is判断是否为T控件
-                return controlDic[controlName][i] as T;
+            //控件并保存在字典中，可根据自己UI搭载的控件更改
+            FinChildControl<Button>();
+            FinChildControl<Image>();
+            FinChildControl<Scrollbar>();
+            FinChildControl<Text>();
+        }
+
+        private void FinChildControl<T>() where T : UIBehaviour
+        {
+            T[] controls = this.GetComponentsInChildren<T>(); //注意用的是GetComents,返回的是一组
+            string objName; //使用名字对控件按物体分类
+            for (int i = 0; i < controls.Length; i++)
+            {
+                objName = controls[i].gameObject.name;
+                if (controlDic.ContainsKey(objName))
+                {
+                    controlDic[objName]
+                        .Add(controls[i]); //注意controlDic中存储的类型为<string, List<UIBehaviour>,所以我们的同一个UI的所有控件都在这个链表中
+                }
+                else
+                {
+                    controlDic.Add(objName, new List<UIBehaviour>() { controls[i] }); //添加UI到字典中
+                }
             }
-        } 
-        return null;
-    }
-
-    //虚函数，可在继承类中自己定义
-    public virtual void ShowMe()
-    {
-      
-    }
-	//同上
-    public virtual void HideMe()
-    {
-      
-    }
+        }
 
 
-}
+        protected T GetControl<T>(string controlName) where T : UIBehaviour
+        {
+            if (controlDic.ContainsKey(controlName))
+            {
+                for (int i = 0; i < controlDic[controlName].Count; i++)
+                {
+                    if (controlDic[controlName][i] is T) //使用is判断是否为T控件
+                        return controlDic[controlName][i] as T;
+                }
+            }
+
+            return null;
+        }
+
+        //虚函数，可在继承类中自己定义
+        public virtual void ShowMe()
+        {
+
+        }
+
+        //同上
+        public virtual void HideMe()
+        {
+
+        }
+
+
+    }
 
 }
