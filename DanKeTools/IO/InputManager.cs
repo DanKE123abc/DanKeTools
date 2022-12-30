7fafd10bf2b5e1c6a83a0bc1a1b37320edc6d7d1
DanKe <danke1024@foxmail.com>
2022-12-30 22:31:34 +0800

    输入管理器检测补全

--- DanKeTools/IO/InputManager.cs
+++ DanKeTools/IO/InputManager.cs
@@ -32,12 +32,79 @@ namespace DanKeTools.IO
 
         private void Update()
         {
-            if (!isStart)
+            if (isStart)
             {
-                return;
-            }
+                #region 按键监听列表
 
-            CheckKeyCode(KeyCode.A);
+                CheckKeyCode(KeyCode.A);
+                CheckKeyCode(KeyCode.B);
+                CheckKeyCode(KeyCode.C);
+                CheckKeyCode(KeyCode.D);
+                CheckKeyCode(KeyCode.E);
+                CheckKeyCode(KeyCode.F);
+                CheckKeyCode(KeyCode.G);
+                CheckKeyCode(KeyCode.H);
+                CheckKeyCode(KeyCode.I);
+                CheckKeyCode(KeyCode.J);
+                CheckKeyCode(KeyCode.K);
+                CheckKeyCode(KeyCode.L);
+                CheckKeyCode(KeyCode.M);
+                CheckKeyCode(KeyCode.N);
+                CheckKeyCode(KeyCode.O);
+                CheckKeyCode(KeyCode.P);
+                CheckKeyCode(KeyCode.Q);
+                CheckKeyCode(KeyCode.X);
+                CheckKeyCode(KeyCode.Y);
+                CheckKeyCode(KeyCode.Z);
+                CheckKeyCode(KeyCode.U);
+                CheckKeyCode(KeyCode.V);
+                CheckKeyCode(KeyCode.W);
+                CheckKeyCode(KeyCode.X);
+                CheckKeyCode(KeyCode.Y);
+                CheckKeyCode(KeyCode.Z);
+                CheckKeyCode(KeyCode.Alpha0);
+                CheckKeyCode(KeyCode.Alpha1);
+                CheckKeyCode(KeyCode.Alpha2);
+                CheckKeyCode(KeyCode.Alpha3);
+                CheckKeyCode(KeyCode.Alpha4);
+                CheckKeyCode(KeyCode.Alpha5);
+                CheckKeyCode(KeyCode.Alpha6);
+                CheckKeyCode(KeyCode.Alpha7);
+                CheckKeyCode(KeyCode.Alpha8);
+                CheckKeyCode(KeyCode.Alpha9);
+                CheckKeyCode(KeyCode.F1);
+                CheckKeyCode(KeyCode.F2);
+                CheckKeyCode(KeyCode.F3);
+                CheckKeyCode(KeyCode.F4);
+                CheckKeyCode(KeyCode.F5);
+                CheckKeyCode(KeyCode.F6);
+                CheckKeyCode(KeyCode.F7);
+                CheckKeyCode(KeyCode.F8);
+                CheckKeyCode(KeyCode.F9);
+                CheckKeyCode(KeyCode.F10);
+                CheckKeyCode(KeyCode.F11);
+                CheckKeyCode(KeyCode.F12);
+                CheckKeyCode(KeyCode.UpArrow);
+                CheckKeyCode(KeyCode.DownArrow);
+                CheckKeyCode(KeyCode.RightArrow);
+                CheckKeyCode(KeyCode.LeftArrow);
+                CheckKeyCode(KeyCode.Escape);
+                CheckKeyCode(KeyCode.LeftShift);
+                CheckKeyCode(KeyCode.RightShift);
+                CheckKeyCode(KeyCode.CapsLock);
+                CheckKeyCode(KeyCode.Tab);
+                CheckKeyCode(KeyCode.KeypadEnter);
+                CheckKeyCode(KeyCode.LeftAlt);
+                CheckKeyCode(KeyCode.RightAlt);
+                CheckKeyCode(KeyCode.LeftControl);
+                CheckKeyCode(KeyCode.RightControl);
+                CheckKeyCode(KeyCode.LeftCommand);
+                CheckKeyCode(KeyCode.RightCommand);
+                CheckKeyCode(KeyCode.Delete);
+                CheckKeyCode(KeyCode.Backspace);
+
+                #endregion
+            }
 
         }
 
