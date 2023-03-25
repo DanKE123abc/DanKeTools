7b280026308641273222c9d522b8a2093bcac266
DanKe <danke1024@foxmail.com>
2023-03-25 23:44:58 +0800

    update

--- Assets/DanKeTools/IO/FileManager.cs
+++ Assets/DanKeTools/IO/FileManager.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 using UnityEngine.Events;
 using DanKeTools;
 using DanKeTools.Mono;
-
+using DanKeTools.Json;
 using System.Text;
 using System.Xml;
 using UnityEngine.U2D;
 
 namespace DanKeTools.IO
 {
-    
+
     ///<summary>
     ///脚本名称： FileManager.cs
     ///修改时间：2022/12/25
@@ -36,7 +36,7 @@ namespace DanKeTools.IO
             sw.Close();
             sw.Dispose();
         }
-        
+
         /// <summary>
         /// 读取文本文件
         /// </summary>
@@ -52,7 +52,7 @@ namespace DanKeTools.IO
             sr.Dispose();
             return info;
         }
-        
+
         /// <summary>
         /// 同步加载资源
         /// </summary>
@@ -99,8 +99,99 @@ namespace DanKeTools.IO
                 //直接传给方法
                 callback(r.asset as T);
             }
-        }  
-        
+        }
+
+        private string dataBasePath = Application.streamingAssetsPath + @"\DataBase\";
+
+        /// <summary>
+        /// 用StreamWriter将string写入文件
+        /// </summary>
+        /// <param name="_name"></param>
+        /// <param name="_object"></param>
+        public void CreateJsonDataBase(string _name, object _object)
+        {
+            if (File.Exists(dataBasePath + _name + ".json"))   // 检查存在
+            {
+                File.Delete(dataBasePath + _name + ".json");
+                Debug.Log(_name + "[FileManager]Json已存在，自动覆盖");
+            }
+            StreamWriter sw = File.CreateText(dataBasePath + _name + ".json");  //如果重名就会创建失败
+            sw.Write(SerializeObject(_object));
+            sw.Close();
+        }
+
+        /// <summary>
+        /// 用StreamReader将读取文件并转为string
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="_name"></param>
+        /// <returns></returns>
+        public T LoadJsonDataBase<T>(string _name)
+        {
+            if (!File.Exists(dataBasePath + _name + ".json")) // 检查存在
+            {
+                Debug.LogError("[FileManager]Json文件不存在");
+                return default(T);
+            }
+            StreamReader sr = File.OpenText(dataBasePath + _name + ".json");
+            string data = sr.ReadToEnd();
+            sr.Close();
+            if (data.Length > 0)    // 检查非空
+                return DeserializeObject<T>(data);
+            return default(T);
+        }
+
+        public void CreateJsonSaveData(string _name, object _object)
+        {
+            if (File.Exists(_name + ".json"))   // 检查存在
+            {
+                File.Delete(_name + ".json");
+                //Debug.Log(_name + ".json已存在，自动覆盖");
+            }
+            StreamWriter sw = File.CreateText(_name + ".json");  //如果重名就会创建失败
+            sw.Write(SerializeObject(_object));
+            sw.Close();
+        }
+
+        public T LoadJsonSaveData<T>(string _name)
+        {
+            if (!File.Exists(_name + ".json"))   // 检查存在
+                return default(T);
+            StreamReader sr = File.OpenText(_name + ".json");
+            string data = sr.ReadToEnd();
+            sr.Close();
+            if (data.Length > 0)    // 检查非空
+                return DeserializeObject<T>(data);
+            return default(T);
+        }
+
+        /// <summary>
+        /// 将对象序列化为string
+        /// </summary>
+        /// <param name="_object"></param>
+        /// <returns></returns>
+        private string SerializeObject(object _object)
+        {
+            string serializedString = string.Empty;
+            serializedString = JsonMapper.ToJson(_object);
+            return serializedString;
+        }
+
+        /// <summary>
+        /// 将string反序列化为对象
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="_string"></param>
+        /// <returns></returns>
+        private T DeserializeObject<T>(string _string)
+        {
+            T deserializedObject = default(T);  //T的默认值
+            deserializedObject = JsonMapper.ToObject<T>(_string);
+            return deserializedObject;
+        }
+
+        #region 小工具
+
         /// <summary>
         /// 加载Sprite
         /// </summary>
@@ -125,7 +216,7 @@ namespace DanKeTools.IO
             if (tempAtlas != null) tempSprite = tempAtlas.GetSprite(spriteName);
             return tempSprite;
         }
-        
+
         /// <summary>
         /// 已加载图集记录
         /// </summary>
@@ -200,8 +291,9 @@ namespace DanKeTools.IO
 
             return content.ToString();
         }
-        
-        
+
+        #endregion
+
     }
-    
+
 }
